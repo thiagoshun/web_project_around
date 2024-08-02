709dc315ba26e22a92c1db77bf03612e5b9912ea
thiagoshun <shun.thiago@gmail.com>
2024-08-03 09:56:42 +1000

    git checkout -b "popup"

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded'), function(){
+
 const popupDados = documento.querySelector('.subheader')
 const button = document.querySelector("subheader__edit")
 const closeButton = document.querySelector('.popup__close-button')
@@ -5,7 +7,7 @@ const closeButton = document.querySelector('.popup__close-button')
 const nameInput = document.querySelector('.subheader__name-input')
 const titleInput = document.querySelector('.subheader__title-title')
 const imageInput = document.querySelector('.subheader__image-image')
-
+}
 console.log(nameInput);
 console.log(titleInput);
 console.log(imageInput);
