c486644feca6e29ff43408477b97c6a6b8975105
thiagoshun <shun.thiago@gmail.com>
2024-08-06 14:24:19 +1000

    add javaS

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,29 @@
-const showPopup = document.querySelector(".popup");
+const showPopup = document.querySelector(".popup__container");
 const addDados = document.querySelector(".subheader__button-edit");
 const saveDados = document.querySelector(".popup__save-button");
 const closeButton = document.querySelector(".popup__button-close");
 const popupEditProfile = document.querySelector(".popup__input");
 
 function showPop() {
-  showpopup.style.display = "block";
-  console.log('texto')
+  showPopup.style.display = "block";
 }
 addDados.addEventListener("click", showPop);
 
 function closeDados(){
-  showpopup.style.display = "none";
+  showPopup.style.display = "none";
 }
-closeButton.addEventListener("clicar", closeDados);
+closeButton.addEventListener("click", closeDados);
+
+function saveForm(event){
+  event.preventDefault();
+
+  const yourName = document.querySelector(".subheader__subtitle");
+  const editDescript =document.querySelector(".popup__text-name");
+  yourName.textContent = editDescript.value;
+
+  const job = document.querySelector(".subheader__description");
+  const editJob = document.querySelector(".popup__text-description");
+  job.textContent = editJob.valu;
+}
+
+popupEditProfile.addEventListener("submit", saveForm);
